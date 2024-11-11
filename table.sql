163b4b8984dc1d5f61622d52e48663689b9cf1b7
Eropso <156892352+Eropso@users.noreply.github.com>
2024-11-11 11:44:55 +0100

    table code

--- table.sql
+++ table.sql
@@ -1,6 +1,39 @@
-CREATE TABLE `businessdb`.`users` (`id` INT NOT NULL 
-AUTO_INCREMENT , `user` VARCHAR(254) NOT NULL , `username` 
-VARCHAR(50) NOT NULL , `password` CHAR(255) NOT NULL , 
-`reg_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
-PRIMARY KEY (`id`), UNIQUE (`user`), UNIQUE (`username`)) 
-ENGINE = InnoDB;
+CREATE TABLE `game_plays` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `user_id` int(11) DEFAULT NULL,
+ `game_name` varchar(255) DEFAULT NULL,
+ `played_at` timestamp NOT NULL DEFAULT current_timestamp(),
+ PRIMARY KEY (`id`),
+ KEY `user_id` (`user_id`),
+ CONSTRAINT `game_plays_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=133 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+
+CREATE TABLE `minesweeper_scores` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `user_id` int(11) NOT NULL,
+ `best_time` int(11) NOT NULL,
+ PRIMARY KEY (`id`),
+ KEY `user_id` (`user_id`),
+ CONSTRAINT `minesweeper_scores_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+
+CREATE TABLE `snake_scores` (
+ `user_id` int(11) NOT NULL,
+ `highest_score` int(11) NOT NULL,
+ PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+
+
+
+CREATE TABLE `users` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `user` varchar(254) NOT NULL,
+ `username` varchar(50) NOT NULL,
+ `password` char(255) NOT NULL,
+ `reg_date` datetime NOT NULL DEFAULT current_timestamp(),
+ PRIMARY KEY (`id`),
+ UNIQUE KEY `user` (`user`),
+ UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
